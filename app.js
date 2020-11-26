11ef470e122dc490729ac9f2e7dc7be6f9e92afe
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-11-26 13:34:39 +0100

    added special attack

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = Vue.createApp({
     data(){
         return{
             playerHealth: 100,
-            monsterHealth: 100
-
+            monsterHealth: 100,
+            currentRound: 0
         };
     },
     computed:{
@@ -16,10 +16,14 @@ const app = Vue.createApp({
         },
         playerBarStyles(){
             return {width: this.playerHealth + '%'};
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
         attackMonster(){
+            this.currentRound++;
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
@@ -27,7 +31,13 @@ const app = Vue.createApp({
         attackPlayer(){
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
-    }
+    },
 });
 app.mount('#game');
